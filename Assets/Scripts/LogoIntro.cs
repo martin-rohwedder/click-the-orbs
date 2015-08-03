63e41d9add58513d4f6ea0809a4d6223964c67b0
Martin Rohwedder <m.rohwedder@gmail.com>
2015-08-03 16:46:17 +0200

    Logo Scene with graphic and sounds added

---
+++ Assets/Scripts/LogoIntro.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+/**
+ * This script will play a fade in and out intro with the Rohwedder Games Logo
+ * 
+ * @author Martin Rohwedder
+ * @version 1.0
+ */
+public class LogoIntro : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private AudioSource[] allAudioSources;
+
+    // Use this for initialization
+    void Awake()
+    {
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+
+        StartCoroutine(FadeInAndOut(GetComponent<SpriteRenderer>()));
+    }
+
+    // Updated every frame
+    void Update()
+    {
+        if (Application.isMobilePlatform)
+        {
+            //Skip the logo intro on screen touch
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    //Load the Main Scene
+                    StopAllAudio();
+                    StartCoroutine(LoadMainScene());
+                }
+            }
+        }
+        else
+        {
+            //Skip the logo intro on mouse click
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Load the Main Scene
+                StopAllAudio();
+                StartCoroutine(LoadMainScene());
+            }
+        }
+    }
+
+    // Fade the sprite in and out
+    private IEnumerator FadeInAndOut(SpriteRenderer sprite)
+    {
+        float duration = 3f; //3 seconds
+        float currentTime = 0f;
+
+        // Alpha values: Fade In : from 0 to 1
+        float oldAlpha = 0.0f;
+        float finalAlpha = 1.0f;
+
+        while (currentTime < duration)
+        {
+            float alpha = Mathf.Lerp(oldAlpha, finalAlpha, currentTime / duration);
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(4.0f);
+
+        currentTime = 0f;
+
+        // Alpha values: Fade Out : from 1 to 0
+        oldAlpha = 1.0f;
+        finalAlpha = 0.0f;
+
+        while (currentTime < duration)
+        {
+            float alpha = Mathf.Lerp(oldAlpha, finalAlpha, currentTime / duration);
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, alpha);
+
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        //Load the Main Scene
+        yield return new WaitForSeconds(1.0f);
+        StartCoroutine(LoadMainScene());
+
+        yield break;
+    }
+
+    void StopAllAudio()
+    {
+        foreach (AudioSource audioSource in allAudioSources)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    IEnumerator LoadMainScene()
+    {
+        float fadeTime = GameObject.Find("_ScreenFader").GetComponent<ScreenFader>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime + 1.0f);
+        Application.LoadLevel(nextSceneName);
+    }
+}
---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour
+{
+
+    public Texture2D fadeOutTexture;
+    public bool fadeInOnLevelLoaded = true;
+    public float fadeTimeMultiplier = 0.5f;
+
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+    private float fadeSpeed = 0.8f;
+
+    void Awake()
+    {
+        if (fadeInOnLevelLoaded)
+        {
+            alpha = 1.0f;
+        }
+        else
+        {
+            alpha = 0.0f;
+        }
+    }
+
+    void OnGUI()
+    {
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade()
+    {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime * fadeTimeMultiplier;
+        alpha = Mathf.Clamp01(alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+        yield return new WaitForSeconds(1.0f);
+        yield break;
+    }
+
+    public float BeginFade(int direction)
+    {
+        fadeDir = direction;
+        return (fadeSpeed);
+    }
+
+    void OnLevelWasLoaded()
+    {
+        BeginFade(-1);
+    }
+
+}
