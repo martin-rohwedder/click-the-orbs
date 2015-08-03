798534cef47102640ce8bf35af3a156ee3287e38
Martin Rohwedder <m.rohwedder@gmail.com>
2015-08-03 09:39:30 +0200

    Start New Game function removed and replaced with a Load Scene function, for minimizing code redundance

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -3,19 +3,9 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    public void StartNewGame()
+    public void LoadScene(string sceneName)
     {
-        Application.LoadLevel("GameScene");
+        Application.LoadLevel(sceneName);
     }
 
     public void QuitGame()
